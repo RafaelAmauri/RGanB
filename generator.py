6089fc5448592a82ec24877666ff67a87bdb2a47
Rafael <amauri.rafael33@gmail.com>
2025-01-12 01:28:06 -0300

    Add new generator version

--- generator.py
+++ generator.py
@@ -66,12 +66,17 @@ class ColorizationCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.leakyReLU = nn.LeakyReLU(0.2)
+        self.relu      = nn.ReLU()
+        self.tanh      = nn.Tanh()
+        self.sigmoid   = nn.Sigmoid()
         
-        self.batchNorm2   = nn.BatchNorm2d(num_features=2)
-        self.batchNorm64  = nn.BatchNorm2d(num_features=64)
-        self.batchNorm128 = nn.BatchNorm2d(num_features=128)
-        self.batchNorm256 = nn.BatchNorm2d(num_features=256)
-        self.batchNorm512 = nn.BatchNorm2d(num_features=512)
+        self.batchNorm3    = nn.BatchNorm2d(num_features=3)
+        self.batchNorm64   = nn.BatchNorm2d(num_features=64)
+        self.batchNorm128  = nn.BatchNorm2d(num_features=128)
+        self.batchNorm256  = nn.BatchNorm2d(num_features=256)
+        self.batchNorm512  = nn.BatchNorm2d(num_features=512)
+        self.batchNorm1024 = nn.BatchNorm2d(num_features=1024)
+
         
         # These will downsize the image
         self.conv1_1    = nn.Conv2d(in_channels=1,  out_channels=64, kernel_size=3, stride=1, padding=1)
@@ -118,9 +123,7 @@ class ColorizationCNN(nn.Module):
         self.conv14    = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.upconv7   = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=2, padding=1, output_padding=1)
 
-        self.conv15    = nn.Conv2d(in_channels=128, out_channels=2, kernel_size=3, stride=1, padding=1)
-        
-        self.conv16    = nn.Conv2d(in_channels=3, out_channels=2, kernel_size=3, stride=1, padding=1)
+        self.conv15    = nn.Conv2d(in_channels=128, out_channels=3, kernel_size=3, stride=1, padding=1)
 
 
     def forward(self, LComponent):
@@ -171,74 +174,69 @@ class ColorizationCNN(nn.Module):
         # Layer 8: [2,2,512] -> [4,4,512]
         layer8 = self.conv8(layer7)
         layer8 = self.upconv1(layer8)
-        layer8 = self.batchNorm512(layer8)
-        layer8 = self.leakyReLU(layer8)
         # Add residuals from Layer 6
         layer8 = torch.cat((layer8, layer6), dim=1)
+        layer8 = self.batchNorm1024(layer8)
+        layer8 = self.relu(layer8)
 
 
         # Layer 9: [4,4,512] -> [8,8,512]
         layer9 = self.conv9(layer8)
         layer9 = self.upconv2(layer9)
-        layer9 = self.batchNorm512(layer9)
-        layer9 = self.leakyReLU(layer9)
         # Add residuals from Layer 5
         layer9 = torch.cat((layer9, layer5), dim=1)
+        layer9 = self.batchNorm1024(layer9)
+        layer9 = self.relu(layer9)
 
 
         # Layer 10: [8,8,512] -> [16,16,512]
         layer10 = self.conv10(layer9)
         layer10 = self.upconv3(layer10)
-        layer10 = self.batchNorm512(layer10)
-        layer10 = self.leakyReLU(layer10)
         # Add residuals from Layer 4
         layer10 = torch.cat((layer10, layer4), dim=1)
+        layer10 = self.batchNorm1024(layer10)
+        layer10 = self.relu(layer10)
 
 
         # Layer 11: [16,16,512] -> [32,32,256]
         layer11 = self.conv11(layer10)
         layer11 = self.upconv4(layer11)
-        layer11 = self.batchNorm256(layer11)
-        layer11 = self.leakyReLU(layer11)
         # Add residuals from Layer 3
         layer11 = torch.cat((layer11, layer3), dim=1)
+        layer11 = self.batchNorm512(layer11)
+        layer11 = self.relu(layer11)
 
 
         # Layer 12: [32,32,256] -> [64,64,128]
         layer12 = self.conv12(layer11)
         layer12 = self.upconv5(layer12)
-        layer12 = self.batchNorm128(layer12)
-        layer12 = self.leakyReLU(layer12)
         # Add residuals from Layer 2
         layer12 = torch.cat((layer12, layer2), dim=1)
+        layer12 = self.batchNorm256(layer12)
+        layer12 = self.relu(layer12)
 
 
         # Layer 13: [64,64,128] -> [128,128,64]
         layer13 = self.conv13(layer12)
         layer13 = self.upconv6(layer13)
-        layer13 = self.batchNorm64(layer13)
-        layer13 = self.leakyReLU(layer13)
         # Add residuals from Layer 1_2
         layer13 = torch.cat((layer13, layer1_2), dim=1)
+        layer13 = self.batchNorm128(layer13)
+        layer13 = self.relu(layer13)
 
 
         # Layer 14: [128,128,64] -> [256,256,64]
         layer14 = self.conv14(layer13)
         layer14 = self.upconv7(layer14)
-        layer14 = self.batchNorm64(layer14)
-        layer14 = self.leakyReLU(layer14)
         # Add residuals from Layer 1_1
         layer14 = torch.cat((layer14, layer1_1), dim=1)
+        layer14 = self.batchNorm128(layer14)
+        layer14 = self.relu(layer14)
 
 
-        # Layer 15: [256,256,64] -> [256,256,2]
+        # Layer 15: [256,256,64] -> [256,256,3]
         layer15 = self.conv15(layer14)
-        layer15 = self.batchNorm2(layer15)
+        #layer15 = self.batchNorm3(layer15)
+        layer15 = self.sigmoid(layer15)
 
-        # Layer 16: [256,256,2] -> [256,256,3]
-        layer16 = torch.cat((layer15, LComponent), dim=1)
-
-        # Layer 17: [256,256,3] -> [256,256,2]
-        layer16 = self.conv16(layer16)
-
-        return layer16
+        return layer15
